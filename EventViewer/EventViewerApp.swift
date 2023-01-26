0787dfac6ab58e1efc8dd534ae539014da8e92eb
Ilya Kharlamov <ilia3546@me.com>
2023-01-26 19:59:04 +0300

    Initial Commit

---
+++ EventViewer/EventViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  EventViewerApp.swift
+//  EventViewer
+//
+//  Created by Ilya Kharlamov on 1/26/23.
+//
+
+import SwiftUI
+
+@main
+struct EventViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
